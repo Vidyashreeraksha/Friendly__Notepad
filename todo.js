dde8802aca66fcc7e829774135ab55817d4a1591
Vidyashreeraksha <62463818+Vidyashreeraksha@users.noreply.github.com>
2020-06-05 20:05:19 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,23 @@
+$("ul").on("click" , "li", function(){
+$(this).toggleClass("completed");
+});
+
+$("ul").on("click", "span", function(){
+$(this).parent().fadeOut(500,function(){
+$(this).remove();
+});
+event.stopPropagation();
+});
+
+$("input[type ='text']").keypress(function(event){
+if (event.which === 13){
+  var todoText = $(this).val();
+  $(this).val("");
+  $("ul").append("<li> <span><i class='far fa-trash-alt'></i></span>" + todoText + "</li>")
+
+}
+});
+
+$(".fa-tasks").click(function(){
+  $("input[type ='text']").fadeToggle();
+});
